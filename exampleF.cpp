5983f85a12709f3cae109840db6cc43c700e29dd
roginn <rogergarcia88@gmail.com>
2012-09-02 18:03:12 -0300

    Improved challenge explanation and F's performance

--- exampleF.cpp
+++ exampleF.cpp
@@ -1,21 +1,53 @@
 #include "F.h"
 
 /*
-F.h provides two functions and one starting value (zero)
-for you to work with:
+Consider the following functions:
+    fun(a,b,c,d) = c if a>b, d otherwise;
+    inc(a) = a+1
 
-    F::fun(F a, F b, F c, F d) 
+Your challenge is to derive the basic mathematical (+, -, *, /, %) and logic
+(&, |, ~) operations using only these 2 functions, recursion and the number 0, 
+
+F.h provides a simple wrapper for you to test your answer. You must always
+work with F objects to represent a number, using:
+
+    F F::fun(const F& a, const F& b, const F& c, const F& d) 
         (returns the equivalent to a > b ? c : d )
-    F::inc(F a)
+    F F::inc(const F& a)
         (returns the equivalent to a++)
-    F::zero
+    F F::zero
         (returns the equivalent to 0)
 
-Your challenge is to derive the operations +-*%/ using only these resources.
-When writing recursive functions, you will probably need a lazy evaluation
-tool. For that, use:
-    F my_function(F arg1, F arg2) 
-        lazy--> F::bind(&my_function, arg1, arg2)
+You will probably need recursion when designing your solution. For instance,
+consider this dummy function:
+
+F my_function(const F& arg1, const F& arg2){
+    return F::fun(
+        arg1,
+        arg2,
+        my_function(arg1, F::zero),
+        F::zero
+    );
+}
+
+C++ will try to evaluate my_function(arg1, F::zero) before it completes the call 
+to F::fun, thus causing a stack overflow. You need a binding mechanism as a lazy
+evaluation wrapper to solve this:
+
+F my_function(const F& arg1, const F& arg2){
+    return F::fun(
+        arg1,
+        arg2,
+        F::bind(&my_function, arg1, F::zero),
+        F::zero
+    );
+}
+
+Then it should work as expected. 
+
+You can also stream your result to std::cout, as in:
+
+std::cout << my_function(arg1, arg2) << std::endl;
 
 */
 
@@ -25,13 +57,13 @@ F two(F::inc(one));
 F three(F::inc(two));
 
 //simple usage of function F::fun
-F dummy0(F x, F y) { return F::fun(x, y, one, zero); }
+F dummy0(const F& x, const F& y) { return F::fun(x, y, one, zero); }
 
 //making compound statements with F::fun
-F dummy1(F x, F y) { return F::fun(x, y, zero, F::fun(y, x, zero, one)); }
+F dummy1(const F& x, const F& y) { return F::fun(x, y, zero, F::fun(y, x, zero, one)); }
 
 //implementing a recursive function with lazy evaluation of itself
-F dummy2(F x, F y) { return F::fun(x, y, F::bind(&dummy2, x, F::inc(y)), x); }
+F dummy2(const F& x, const F& y) { return F::fun(x, y, F::bind(&dummy2, x, F::inc(y)), x); }
 
 int main(){
 
